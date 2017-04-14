caffe775839ed2ea2a20e2cac386792400549bd4
Rishiraj Upadhyay <rishiraj.upadhyay@raweng.com>
2017-04-14 18:16:12 +0530

    added cdn in host

--- public/js/csbscript.js
+++ public/js/csbscript.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-
 	$(".js-example-basic-single").select2();
-
 });
-
+$(window).on('load resize', function () {
+	var equalheight = $('.content-env-sec .styled-select label'),
+		windowSize = $(window).width();
+	if (windowSize <= 885 && windowSize >= 767 ){
+		equalheight.css('height','45px');
+	}else{
+		equalheight.css('height','auto');
+	}
+});
